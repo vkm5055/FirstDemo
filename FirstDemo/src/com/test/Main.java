989a709f879b218f7197ba03dc1dd1f0e4dc5c7a
VIKASH <vikash@gmail.com>
2017-12-01 13:31:37 +0530

    Initial commit

---
+++ FirstDemo/src/com/test/Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Main {
+	public static void main(String[] args) {
+		display();
+	}
+	
+	public static void display(){
+		System.out.println("First Line Code.");
+		System.out.println("Second Line Code.");
+		
+		
+	}
+}
